589cb02c76272c951170c64460db0434a42c74ad
alan-404 <nguyentri.alan@gmail.com>
2023-08-19 12:35:38 +0700

    add comments

--- api.py
+++ api.py
@@ -8,6 +8,7 @@ import numpy as np
 import re
 import time
 
+# Get Information about Server
 parser = ArgumentParser()
 parser.add_argument('--host', default='127.0.0.1', help='Host IP to bind to')
 parser.add_argument('--port', type=int, default=8000, help='Port to listen on')
@@ -17,27 +18,35 @@ parser.add_argument('--max_ctx', type=int, default=201)
 parser.add_argument("--device", type=str, default='cpu')
 args = parser.parse_args()
 
+# Config App and APIs
 app = FastAPI()
 
+# Device Config and Load ONNX Model
 if args.device.lower() != "cpu" and ort.get_device() == 'GPU':
     model = ort.InferenceSession(args.model, providers=['CUDAExecutionProvider'])
 else:
     model = ort.InferenceSession(args.model, providers=['CPUExecutionProvider'])
 
+# Load Tokenizer
 tokenizer = Tokenizer(args.tokenizer)
 end_token = tokenizer.get_special_token("end")
 
+# DTO
 class ChatMessage(BaseModel):
     message: str
 
+# API
 @app.post("/chat")
 def hello(chat_message: ChatMessage):
     start_time = time.time()
     request_text_length = len(chat_message.message.split(" "))
+
+    # Pre-Process Textual Data
     digit = tokenizer.text_to_sequences([chat_message.message], start_token=True, sep_token=True)
     request_token_length = digit.shape[1]
     response_start_index = digit.shape[-1]
 
+    # Generate Token Stage
     for _ in range(args.max_ctx):
         output = model.run(
             None,
@@ -50,6 +59,7 @@ def hello(chat_message: ChatMessage):
 
         digit = np.concatenate((digit, np.expand_dims(pred_token, axis=0)), axis=-1)
 
+    # Post-Process Data
     texts = []
     response_token_length = len(digit[0][response_start_index:])
     for item in digit[0][response_start_index:]:
@@ -60,6 +70,8 @@ def hello(chat_message: ChatMessage):
     response_text_length = len(response.split(" "))
     response = response.strip()
     end_time = time.time()
+
+    # Response
     return {'response': response, 
             "time_out": end_time-start_time,
             "request_text_length": request_text_length,
@@ -68,5 +80,7 @@ def hello(chat_message: ChatMessage):
             "response_token_length": response_token_length}
 
 
+
+# Start FastAPI App
 if __name__ == '__main__':
     uvicorn.run(app, host=args.host, port=args.port)
--- build.py
+++ build.py
@@ -5,22 +5,29 @@ import os
 
 
 def build_model(checkpoint: str, tokenizer_path: str, build_path: str, device: str):
-    if os.path.exists(checkpoint) == False or os.path.exists(tokenizer_path) == False:
-        return
+    # Check Config Paths
+    assert os.path.exists(checkpoint) == True and os.path.exists(tokenizer_path) == True
+    
+    # Handle Device
     if device != 'cpu' and torch.cuda.is_available() == False:
         device = 'cpu'
     
+    # Load Tokenizer
     tokenizer = Tokenizer(tokenizer_path)
 
+    # Load Trainer of Model
     trainer = GPTTrainer(
         token_size=len(tokenizer.dictionary),
         device=device,
         checkpoint=checkpoint
     )
+    # Set Model Mode to Evaludation
     trainer.model.eval()
 
+    # Init input of Model
     dummpy_input = torch.randint(low=0, high=len(tokenizer.dictionary), size=(1, 20))
 
+    # Export model to ONNX
     torch.onnx.export(trainer.model, dummpy_input.to(device), build_path, input_names=["input"], output_names=['output'], dynamic_axes={"input": {0: 'batch_size', 1: "n_ctx"}, "output": {0: "batch_size", 1: "n_ctx"}})
 
 if __name__ == '__main__':
